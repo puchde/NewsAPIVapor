dc402a5930cc6c08ad0378297bd313cff5d69712
Willy <puchwilly@airiti.com>
2024-03-28 16:40:06 +0800

    feat: 新增套件FCM

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.83.1"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
-        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.25.2")
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.25.2"),
+        .package(url: "https://github.com/MihaelIsaev/FCM.git", from: "2.13.0")
     ],
     targets: [
         .executableTarget(
@@ -18,7 +19,8 @@ let package = Package(
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "SwiftSoup", package: "SwiftSoup"),
-                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "FCM", package: "FCM")
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
@@ -28,7 +30,8 @@ let package = Package(
             // Workaround for https://github.com/apple/swift-package-manager/issues/6940
             .product(name: "Vapor", package: "vapor"),
             .product(name: "SwiftSoup", package: "SwiftSoup"),
-            .product(name: "SwiftProtobuf", package: "swift-protobuf")
+            .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+            .product(name: "FCM", package: "FCM")
         ])
     ]
 )
